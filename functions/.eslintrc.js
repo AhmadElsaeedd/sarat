718b7f41d8268b04184acd4961811dec71c897f2
Ahmad Elsaeed <ae2200@nyu.edu>
2023-12-23 17:59:51 +0200

    whatsapp api simply working

---
+++ functions/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  parserOptions: {
+    "ecmaVersion": 2018,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    "no-restricted-globals": ["error", "name", "length"],
+    "prefer-arrow-callback": "error",
+    "quotes": "off",
+    "camelcase": "off",
+    "require-jsdoc": "off",
+    "max-len": "off",
+    "no-constant-condition": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
+  globals: {},
+};
