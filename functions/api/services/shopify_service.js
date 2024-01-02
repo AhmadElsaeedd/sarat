3cce7999bd3230b69154f8f345afbd9a7a9b9ca3
Ahmad Elsaeed <ae2200@nyu.edu>
2024-01-02 23:08:58 +0200

    very simple implementation of getting abandoned carts

--- functions/api/services/shopify_service.js
+++ functions/api/services/shopify_service.js
@@ -1,21 +1,32 @@
-require('@shopify/shopify-api/adapters/node');
+// require('@shopify/shopify-api/adapters/node');
+const axios = require('axios');
+// const {Shopify} = require('@shopify/shopify-api');
 
-const {shopifyApi, LATEST_API_VERSION, Shopify} = require('@shopify/shopify-api');
-
-const shopify = shopifyApi({
-  // The next 4 values are typically read from environment variables for added security
-  apiKey: "ef3aa22bb5224ece6ac31306731ff62d",
-  apiSecretKey: "44095502e2626466960c924e4af35e7e",
-  scopes: ['read_products', 'read_customers'],
-  isEmbeddedApp: false,
-  hostName: "https://textlet0.retool.com/apps/6bfe02fa-a7c4-11ee-bc9e-37095146c11a/Textlet%20dashboard",
-  apiVersion: LATEST_API_VERSION, // Use the latest API version
-});
+// const shopify = shopifyApi({
+//   // The next 4 values are typically read from environment variables for added security
+//   apiKey: "ef3aa22bb5224ece6ac31306731ff62d",
+//   apiSecretKey: "44095502e2626466960c924e4af35e7e",
+//   scopes: ['read_products', 'read_customers'],
+//   isEmbeddedApp: false,
+//   hostName: "https://textlet0.retool.com/apps/6bfe02fa-a7c4-11ee-bc9e-37095146c11a/Textlet%20dashboard",
+//   apiVersion: LATEST_API_VERSION, // Use the latest API version
+// });
 
 async function get_abandoned_orders(shop, access_token) {
-  const session = new Shopify.Session.Session(shop, access_token);
-  const client = new shopify.clients.Rest({session});
-  console.log("client is: ", client);
+  const url = `https://${shop}/admin/api/2023-10/checkouts.json?limit=1`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'X-Shopify-Access-Token': access_token,
+      },
+    });
+    console.log("response data is: ", response.data);
+    return response.data; // or process the data as needed
+  } catch (error) {
+    console.error('Error fetching abandoned orders:', error);
+    throw error;
+  }
 }
 
 module.exports = {get_abandoned_orders};
