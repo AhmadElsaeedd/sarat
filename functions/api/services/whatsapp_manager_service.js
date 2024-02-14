120af0ada371cbaf9d8f1e012983ec482cbc0298
Ahmad Elsaeed <ae2200@nyu.edu>
2024-02-14 18:32:11 +0400

    creation of message templates happens correctly now

--- functions/api/services/whatsapp_manager_service.js
+++ functions/api/services/whatsapp_manager_service.js
@@ -36,8 +36,10 @@ function createHash(input) {
   return crypto.createHash('md5').update(input).digest('hex').substring(0, 4);
 }
 
-async function create_session(access_token) {
-  const url = `https://graph.facebook.com/v19.0/834177964858133/uploads?file_length=4446962&file_type=image/jpg&access_token=${access_token}`;
+async function create_session(access_token, filePath) {
+  const stats = fs.statSync(filePath);
+  const fileSizeInBytes = stats.size;
+  const url = `https://graph.facebook.com/v19.0/834177964858133/uploads?file_length=${fileSizeInBytes}&file_type=image/jpg&access_token=${access_token}`;
 
   let response;
   try {
@@ -70,26 +72,8 @@ async function downloadImage(imageUrl) {
   });
 }
 
-// async function uploadImage(filePath, uploadUrl, accessToken) {
-//   const fileStream = fs.createReadStream(filePath);
-
-//   const response = await axios({
-//     method: 'post',
-//     url: uploadUrl,
-//     data: fileStream,
-//     headers: {
-//       'Authorization': `OAuth ${accessToken}`,
-//       'file_offset': '0',
-//     },
-//   });
-
-//   return response.data;
-// }
-
 async function uploadImage(uploadSessionId, accessToken, filePath) {
   const url = `https://graph.facebook.com/v19.0/${uploadSessionId}`;
-  console.log("FILE PATH IS: ", filePath);
-  // const fileStream = fs.createReadStream(filePath);
   const fileData = fs.readFileSync(filePath);
 
   try {
@@ -113,28 +97,23 @@ async function uploadImage(uploadSessionId, accessToken, filePath) {
   }
 }
 
-async function initiate_upload(access_token, session_id) {
-  const imageUrl = 'https://cdn.shopify.com/s/files/1/0676/9600/1322/files/NOR4161.jpg?v=1704811249';
-  const filePath = await downloadImage(imageUrl);
-  console.log("File path is: ", filePath);
+async function initiate_upload(access_token, session_id, filePath) {
   let response;
   try {
     response = await uploadImage(session_id, access_token, filePath);
-    console.log(response);
   } catch (error) {
     console.error('Error uploading image:', error);
   }
-  console.log("Reponse of upload image: ", response);
 
   return response;
 }
 
 async function create_message_templates(shop, segment_number) {
   const keys = await firebase_service.get_whatsapp_keys(shop);
-  const session_id = await create_session(keys.whatsapp_access_token);
-  console.log("Session id created: ", session_id);
-  const file_handle = await initiate_upload(keys.whatsapp_access_token, session_id.id);
-  console.log("File handle is: ", file_handle);
+  const imageUrl = 'https://cdn.shopify.com/s/files/1/0676/9600/1322/files/NOR4161.jpg?v=1704811249';
+  const filePath = await downloadImage(imageUrl);
+  const session_id = await create_session(keys.whatsapp_access_token, filePath);
+  const file_handle = await initiate_upload(keys.whatsapp_access_token, session_id.id, filePath);
   const url = `https://graph.facebook.com/v19.0/${keys.whatsapp_business_account_id}/message_templates`;
   const headers = {
     'Authorization': `Bearer ${keys.whatsapp_access_token}`,
@@ -157,7 +136,7 @@ async function create_message_templates(shop, segment_number) {
             "header_handle": [
             // file handle here from the resumable API
               // "4::aW1hZ2UvanBn:ARZ-U8u7pabJA-3s__BxLqOterhWXXR30-QrqGtopgTM2KqydsCCyRNk0lRDBbXFBR8QRPaiY0m7iDqfj4OiDzShydNd4eZjp14AU-GwoXAfaw:e:1707826616:834177964858133:61552296369370:ARZHHa4LCgulCQPQ4mU\n4::aW1hZ2UvanBn:ARY_Nkkg1957assHKcCjOo-e-OqlDiw2qyqRr_jEASS5Lqo5eYwdpY3XFwZW-hjODLYIV_qaVgz0_2kAE9NPgLqZsaiRcddbsCWG-uahAajmYA:e:1707826616:834177964858133:61552296369370:ARawDpm176-76T1JUcE\n4::aW1hZ2UvanBn:ARaSeIDKd_8WvXxwAEd0aQtT0azOtn-RmUuhFE228psrIeQFumzal4Xwy8y8rsInBxKvMFLZG8mBvEDrCbuyGw2crE5uTdXF4mz9Xqfsl1SQXQ:e:1707826617:834177964858133:61552296369370:ARYNYwAzOwPeLRbqTxQ\n4::aW1hZ2UvanBn:ARZUDPmBQpEH4eP9I_sKH3pUF4rPkYcHO5xcb8ByhIzZi6mXAZleLX-pNx7gkypAyPIwGEm45k4GjlzinMj_GUdm9x0zkl61X3CchCIABjNsrw:e:1707826617:834177964858133:61552296369370:ARZqpnZ1pDv65gW03IU\n4::aW1hZ2UvanBn:ARbn9L7e8RgQ8uItI-qXuIXnzPcawwaE34xvu1dWV1UQE64WDvKyer0vTvfB-sjEyZ5w4qtsKYx68ZpavbDfKNPa9BqkujVkWHRrXzWyUPaDzA:e:1707826617:834177964858133:61552296369370:ARYQLu5trcEKsqiVLJg",
-              file_handle,
+              file_handle.h,
             ],
           },
         },
