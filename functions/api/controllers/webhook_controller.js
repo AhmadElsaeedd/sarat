0f46159917a46c541848c59f1d547d66f3335916
Ahmad Elsaeed <ae2200@nyu.edu>
2024-02-25 16:52:27 +0400

    setup for listening to freshchat webhooks

--- functions/api/controllers/webhook_controller.js
+++ functions/api/controllers/webhook_controller.js
@@ -18,56 +18,11 @@ const getWebhook = (req, res) => {
   }
 };
 
-// const postWebhook = async (req, res) => {
-//   try {
-//     const entries = req.body.entry;
-//     if (entries && entries.length) {
-//       // Process each entry asynchronously
-//       const processingEntries = entries.map(async (entry) => {
-//         const changes = entry.changes;
-//         if (changes && changes.length) {
-//           // Process each change asynchronously
-//           const processingChanges = changes.map(async (change) => {
-//             if (change.value && change.value.messages && change.value.messages.length) {
-//               // Process each message asynchronously
-//               const processingMessages = change.value.messages.map(async (message) => {
-//                 // ToDo: get the phone number of the user who sent the message
-//                 const userPhone = message.from;
-//                 const message_text = message.text.body;
-//                 // Have to check whether this is a duplicate message or not using this ID!
-//                 const messageId = message.id;
-//                 const current_shop = await firebase_service.get_users_conversation(userPhone);
-//                 const message_is_duplicate = await firebase_service.does_message_exist(current_shop, messageId);
-//                 if (message_is_duplicate) {
-//                   console.log("Duplicate message received, ignoring...");
-//                   return;
-//                 }
-//                 console.log("Received message from", userPhone, ": ", message_text);
-//                 await main_control.main_control(userPhone, message_text, messageId);
-//                 console.log("SENT MESSAGE");
-//               });
-
-//               await Promise.all(processingMessages);
-//             }
-//           });
-
-//           await Promise.all(processingChanges);
-//         }
-//       });
-
-//       await Promise.all(processingEntries);
-//     }
-
-//     res.status(200).send('EVENT RECEIVED');
-//   } catch (error) {
-//     console.error("Error in postWebhook:", error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// };
-
 const postWebhook = async (req, res) => {
   try {
     console.log("Received a webhook: ", req.body);
+    console.log("Webhook freshchat header is: ", req.headers['x-freshchat-signature']);
+    // Now do something specific for messages that are coming from the freshchat webhook
     const entries = req.body.entry;
     if (entries && entries.length) {
       // Process each entry asynchronously
